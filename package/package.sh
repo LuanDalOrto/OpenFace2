ec993944d60682b29adfb4da2e25ea4f0aa6f97d
Wenwei Huang <dut.hww@gmail.com>
2020-12-21 19:08:57 +0800

    Fix install prefix

--- package/package.sh
+++ package/package.sh
@@ -15,13 +15,13 @@ package() {
         --exclude "package" \
         $BASEDIR/../ $BASEDIR/src/$reponame
 
-    local install_dir=/usr/local
+    local install_prefix=/usr/local
     get_version $1
 
 	pushd $BASEDIR/src/$reponame >/dev/null
         mkdir -p build
         cd build
-        cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=$install_dir -D CMAKE_CXX_FLAGS="-std=c++11" -D CMAKE_EXE_LINKER_FLAGS="-std=c++11" ..
+        cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=$install_prefix -D CMAKE_CXX_FLAGS="-std=c++11" -D CMAKE_EXE_LINKER_FLAGS="-std=c++11" ..
         make -j8
         make install
 	popd >/dev/null
@@ -35,17 +35,17 @@ package() {
         --deb-no-default-config-files \
         -p $BASEDIR/${name}_VERSION_ARCH.deb \
         -d "libopenblas-dev" \
-        $install_dir/bin/FaceLandmarkImg=${install_prefix}/bin/ \
-        $install_dir/bin/FaceLandmarkVid=${install_prefix}/bin/ \
-        $install_dir/bin/FaceLandmarkVidMulti=${install_prefix}/bin/ \
-        $install_dir/bin/FeatureExtraction=${install_prefix}/bin/ \
-        $install_dir/etc/OpenFace=${install_prefix}/etc/ \
-        $install_dir/include/OpenFace=${install_prefix}/include/ \
-        $install_dir/lib/cmake/OpenFace=${install_prefix}/lib/cmake/ \
-        $install_dir/lib/libFaceAnalyser.a=${install_prefix}/lib/ \
-        $install_dir/lib/libGazeAnalyser.a=${install_prefix}/lib/ \
-        $install_dir/lib/libLandmarkDetector.a=${install_prefix}/lib/ \
-        $install_dir/lib/libUtilities.a=${install_prefix}/lib/
+        $install_prefix/bin/FaceLandmarkImg=${install_prefix}/bin/ \
+        $install_prefix/bin/FaceLandmarkVid=${install_prefix}/bin/ \
+        $install_prefix/bin/FaceLandmarkVidMulti=${install_prefix}/bin/ \
+        $install_prefix/bin/FeatureExtraction=${install_prefix}/bin/ \
+        $install_prefix/etc/OpenFace=${install_prefix}/etc/ \
+        $install_prefix/include/OpenFace=${install_prefix}/include/ \
+        $install_prefix/lib/cmake/OpenFace=${install_prefix}/lib/cmake/ \
+        $install_prefix/lib/libFaceAnalyser.a=${install_prefix}/lib/ \
+        $install_prefix/lib/libGazeAnalyser.a=${install_prefix}/lib/ \
+        $install_prefix/lib/libLandmarkDetector.a=${install_prefix}/lib/ \
+        $install_prefix/lib/libUtilities.a=${install_prefix}/lib/
 
     rm -r $BASEDIR/src
 }
